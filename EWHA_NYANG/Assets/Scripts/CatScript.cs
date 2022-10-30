db7ac65730cd9e14609ba931e7272a120083e8b4
lucykorea414 <31093793+lucykorea414@users.noreply.github.com>
2022-10-30 22:29:48 +0900

    build test

--- EWHA_NYANG/Assets/Scripts/CatScript.cs
+++ EWHA_NYANG/Assets/Scripts/CatScript.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatScript : MonoBehaviour
 {
+    public Text text;
+    //public GameObject cat;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,34 @@ public class CatScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            Physics.Raycast(ray, out hit);
+
+            if (hit.collider.tag == "cat")
+            {
+                text.text = "포획 성공!";
+                hit.collider.gameObject.SetActive(false);
+            }
+
+        }
     }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    Touch touch = Input.GetTouch(0);
+    //    RaycastHit hit;
+    //    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+    //    Physics.Raycast(ray, out hit);
+
+    //    if (hit.collider.tag == "cat")
+    //    {
+    //        text.text = "포획 성공!";
+    //        Destroy(collision.gameObject);
+    //        gameObject.SetActive(false);
+    //    }
+    //}
 }
